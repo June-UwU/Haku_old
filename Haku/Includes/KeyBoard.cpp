ac7fc7c5329cc7bd0af2247f49e624ecb3c4891d
June <williamreji66@gmail.com>
2021-08-13 08:36:14 -0700

    buffered keyboard

--- Haku/Includes/KeyBoard.cpp
+++ Haku/Includes/KeyBoard.cpp
@@ -2,15 +2,41 @@
 
 void KeyBoardEvents::KeyPress(int KeyCode) noexcept
 {
+	TrimBuffer(EventQueue);
+	EventQueue.push(Events(KeyCode, KeyBoardEvents::Events::Type::Keypress));
 	KeyState.set(KeyCode);
 }
 
 void KeyBoardEvents::KeyRelease(int KeyCode) noexcept
 {
+	TrimBuffer(EventQueue);
+	EventQueue.push(Events(KeyCode, KeyBoardEvents::Events::Type::Keyrelease));
 	KeyState.reset(KeyCode);
 }
 
 bool KeyBoardEvents::CheckKeyDown(int KeyCode) noexcept
 {
 	return KeyState[KeyCode];
+}
+
+KeyBoardEvents::Events KeyBoardEvents::GetEvents() noexcept
+{
+	if (EventQueue.size() > 0)
+	{
+		Events ret = EventQueue.front();
+		EventQueue.pop();
+		return ret;
+	}
+	else
+	{
+		return Events(0, KeyBoardEvents::Events::Type::NOEVENT);
+	}
+}
+
+void KeyBoardEvents::TrimBuffer(std::queue<Events>& Ref) noexcept
+{
+	while (Ref.size() > MAXQUEUESIZE)
+	{
+		Ref.pop();
+	}
 }
--- Haku/Includes/KeyBoard.h
+++ Haku/Includes/KeyBoard.h
@@ -1,13 +1,37 @@
 #pragma once
 #include <bitset>
+#include <queue>
+
 class KeyBoardEvents
 {
+private:
+	class Events
+	{
+	public:
+		enum class Type
+		{
+			Keypress,
+			Keyrelease,
+			NOEVENT
+		};
+		Events(int KeyCode, Type type)
+			: KeyCode(KeyCode)
+			, type(type)
+		{
+		}
+		Type type;
+		int	 KeyCode;
+	};
+	void TrimBuffer(std::queue<Events>& Ref) noexcept;
+	;
+	static constexpr int MAXQUEUESIZE = 16;
+	std::bitset<256u>	 KeyState{};
+	std::queue<Events>	 EventQueue;
+
 public:
 	KeyBoardEvents() = default;
-	void KeyPress(int KeyCode) noexcept;
-	void KeyRelease(int KeyCode) noexcept;
-	bool CheckKeyDown(int KeyCode) noexcept;
-
-private:
-	std::bitset<256u> KeyState{};
+	void   KeyPress(int KeyCode) noexcept;
+	void   KeyRelease(int KeyCode) noexcept;
+	bool   CheckKeyDown(int KeyCode) noexcept;
+	Events GetEvents() noexcept;
 };
